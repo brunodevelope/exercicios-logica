ecb310bef88fe43571ea07b550bde47e6a9dac84
Your Namecls <brunodevelope>
2023-01-07 00:55:32 -0300

    "Enviando commit "

--- Algoritmo/src/exericicios/logica/Exercicio01.java
+++ Algoritmo/src/exericicios/logica/Exercicio01.java
@@ -1,7 +1,12 @@
 package exericicios.logica;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
  public static void main(String[] args) {
-	System.out.println("ola");
+	System.out.println("Informe Seu Nome");
+	String nome  = new Scanner(System.in).nextLine();
+	
+	System.out.println("ola tudo bem " + nome);
 }
 }
